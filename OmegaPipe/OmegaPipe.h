8181b939235734c4a6ec7322b8c1f855e5b7f526
tokijh <tokijh@naver.com>
2019-02-20 09:41:16 +0900

    Initial Commit

---
+++ OmegaPipe/OmegaPipe.h
@@ -0,0 +1,19 @@
+//
+//  OmegaPipe.h
+//  OmegaPipe
+//
+//  Created by 윤중현 on 20/02/2019.
+//  Copyright © 2019 tokijh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OmegaPipe.
+FOUNDATION_EXPORT double OmegaPipeVersionNumber;
+
+//! Project version string for OmegaPipe.
+FOUNDATION_EXPORT const unsigned char OmegaPipeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmegaPipe/PublicHeader.h>
+
+
